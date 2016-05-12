5258fc4776c2feb8f52f7c2935f761beb1876138
taylorbenna <taylor@benna.ca>
2016-05-12 14:31:52 -0700

    first commit

---
+++ FastestRPM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FastestRPM
+//
+//  Created by Taylor Benna on 2016-05-12.
+//  Copyright © 2016 Taylor Benna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
